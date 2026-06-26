97b2b92308e615b6584483add5c416b5d80ade10
fadilsyhptra <fadilsyh26@gmail.com>
2026-06-26 21:52:16 +0700

    bug fixed

--- assets/js/checkpoint.js
+++ assets/js/checkpoint.js
@@ -1,8 +1,18 @@
 function onTurnstileSuccess(token) {
+    console.log("Turnstile sukses!");
+    console.log("Token:", token);
+
     let expiry = new Date();
-    expiry.setTime(expiry.getTime() + (1 * 60 * 1000));
-    
-    document.cookie = "full_screen_verified=true; expires=" + expiry.toUTCString() + "; path=/; samesite=strict";
-    window.location.replace('/');
+    expiry.setTime(expiry.getTime() + 60 * 1000);
+
+    document.cookie =
+        "full_screen_verified=true; expires=" +
+        expiry.toUTCString() +
+        "; path=/; SameSite=Lax";
+
+    console.log("Cookie setelah set:", document.cookie);
+
+    // window.location.replace("/");
 }
+
 window.onTurnstileSuccess = onTurnstileSuccess;
